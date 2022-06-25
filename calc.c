bad22754e49983ea0897c0dd90b1c7b0e2355365
dinesh-58 <gurungsujal@hotmail.com>
2022-06-25 10:12:31 +0545

    Create functional code for option 1

--- calc.c
+++ calc.c
@@ -39,7 +39,7 @@ void menu() {
   printf("\nEnter choice ");
 
   a = getch();
-  a == '1' ? wake_x() 
+    a == '1' ? get_time(), input_time(), wake_x() 
   : a == '2' ? get_time(), input_time(), bed(&custom_time) 
   : a == '3' ? get_time(), bed(p_cur_time) 
   : a == '4' ? get_time(), power_nap()
@@ -56,12 +56,25 @@ void menu() {
 }
 void wake_x()  // I want to wake at the following time
 {
-  printf("\nInput time in format like 7:10 am  ");
-  // difftime(input_time, EPOCH value of ideal sleep cycles). show as long as
-  // EPOCH value greater than cur_time for loop's range might have to start
-  // from 1. instead of constantly printing get more sleep, print line at  6
-  // cycles or last one to say to aim for this much or more sleep
+  int i;
+  char buff[9];
+  // Ironically I am using wake_time struct to store ideal time to sleep.
+  printf("Go to bed at: \n");
+  for (i=4; i<=6; i++)
+  {
+    wake_time.tm_hour = custom_time.tm_hour;
+    wake_time.tm_min = custom_time.tm_min - (i * 90) - time_to_sleep;
+
+    if (_mktime64(p_wake_time) > _mktime64(p_cur_time)) {
+      //To only print a time value if it is past current time
+      strftime(buff, 9, "%I:%M %p", p_wake_time);
+      printf("\n %s \t (%.1f hours)", buff, i * 1.5);
+      
+      if (i == 4) printf("\t Try sleeping more than this");
+    }
+  }  
 }
+
 void input_time() {
   char am_pm[3];
   int h, m;
@@ -79,6 +92,7 @@ void input_time() {
   custom_time.tm_min = m;
   _mktime64(&custom_time);
 }
+
 void get_time() {
   time_t my_time = time(NULL);  // gets system time in EPOCH format
   p_cur_time = localtime(&my_time);
@@ -103,7 +117,7 @@ void bed(struct tm *p_temp_time) {
     _mktime64(&wake_time);
     strftime(buff, 9, "%I:%M %p", p_wake_time);
 
-    printf("\n%s\t(%.1f hours)", buff, i * 1.5);
+    printf("\n %s \t (%.1f hours)", buff, i * 1.5);
     if (i == 4) printf(" Try sleeping more than this.");
   }
 }
